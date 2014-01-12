19e79bc87a90e495a856587edde77cac5ef76438
Rebecca Paz <r.pazyaquian@gmail.com>
2014-01-12 11:18:35 -0500

    let it crash

--- routes.py
+++ routes.py
@@ -44,11 +44,12 @@ def lookup():
 
     print "building plots..."
     for i in symbols_list:
-        try:
-            snippet_dict[i] = plots.build_plot(i.upper())
-        except IOError:
-            print "symbol %s not found" % i
-            continue
+        snippet_dict[i] = plots.build_plot(i.upper())
+        #try:
+        #    snippet_dict[i] = plots.build_plot(i.upper())
+        #except IOError:
+        #    print "symbol %s not found" % i
+        #    continue
 
     print "rendering template..."
 
