cfe13dc5758bbad66aff1ddb3e3c269e630d8b4f
Rebecca Paz <r.pazyaquian@gmail.com>
2014-01-13 23:54:30 -0500

    removing exception block

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import plots
+import traceback
 
 app = Flask(__name__)
 
@@ -44,11 +45,13 @@ def lookup():
 
     print "building plots..."
     for i in symbols_list:
-        try:
-            snippet_dict[i] = plots.build_plot(i.upper())
-        except IOError:
-            print "symbol %s not found" % i
-            continue
+        snippet_dict[i] = plots.build_plot(i.upper())
+        #try:
+        #    snippet_dict[i] = plots.build_plot(i.upper())
+        #except IOError, e:
+        #    print "symbol %s not found" % i
+        #    traceback.print_exc()
+        #    continue
 
     print "rendering template..."
 
